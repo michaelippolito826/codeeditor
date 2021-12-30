8392eb143c0eb64b898d5b457319828f77f9951b
michaelippolito826 <michaelippolito826@gmail.com>
2021-12-29 23:44:45 -0500

    added file changing

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-editor = ace.edit("editor")
+editor = ace.edit("editor");
 editor.session.setMode("ace/mode/html");
 editor.setTheme("ace/theme/one_dark");
-const fs = require('fs')
+const fs = require("fs");
 
 editor.setOptions({
-	fontSize: "15pt",
-	useSoftTabs: true,
-	enableEmmet: true,
-    });
+  fontSize: "15pt",
+  useSoftTabs: true,
+  enableEmmet: true,
+});
 
 //don't touch
 var Emmet = require("ace/ext/emmet");
@@ -16,36 +16,79 @@ editor.setOption("enableEmmet", true);
 //editor.setKeyboardHandler("ace/keyboard/vscode")
 
 editor.commands.addCommand({
-        name: "showKeyboardShortcuts",
-        bindKey: {win: "Ctrl-Alt-h", mac: "Command-Alt-h"},
-        exec: function(editor) {
-            ace.config.loadModule("ace/ext/keybinding_menu", function(module) {
-                module.init(editor);
-                editor.showKeyboardShortcuts()
-            })
-        }
-      })
+  name: "showKeyboardShortcuts",
+  bindKey: { win: "Ctrl-Alt-h", mac: "Command-Alt-h" },
+  exec: function (editor) {
+    ace.config.loadModule("ace/ext/keybinding_menu", function (module) {
+      module.init(editor);
+      editor.showKeyboardShortcuts();
+    });
+  },
+});
 
 function addhtmlfile() {
-  let filesdiv = document.getElementById("files")
-  let filename = prompt('Name of new HTML file!');
-  let btn = document.createElement("button");
-  btn.innerHTML = filename + ".js";
-  filesdiv.appendChild(btn);
+  let filesdiv = document.getElementById("files");
+  let filename = prompt("Name of new HTML file!");
+  if (filename !== null) {
+    let btn = document.createElement("button");
+    btn.className = "filebuttons";
+    btn.onclick = function () {
+      editor.setSession(htmlfile);
+      editor.session.setMode("ace/mode/html");
+      var Emmet = require("ace/ext/emmet");
+      editor.setOption("enableEmmet", true);
+    };
+    btn.innerHTML = filename + ".html";
+    filesdiv.appendChild(btn);
 
+    let htmlfile = ace.createEditSession("html");
+    editor.setSession(htmlfile);
+    editor.session.setMode("ace/mode/html");
+  }
+}
 
-  let data = "Learning how to write in a file."
-  fs.writeFile('Output.txt', data, (err) => {
-      
-    // In case of a error throw err.
-    if (err) throw alert(e);
-  })
+function addcssfile() {
+  let filesdiv = document.getElementById("files");
+  let filename = prompt("Name of new CSS file!");
+  if (filename !== null) {
+    let btn = document.createElement("button");
+    btn.className = "filebuttons";
+    btn.onclick = function () {
+      editor.setSession(cssfile);
+      editor.session.setMode("ace/mode/css");
+    };
+    btn.innerHTML = filename + ".css";
+    filesdiv.appendChild(btn);
+
+    let cssfile = ace.createEditSession("css");
+    editor.setSession(cssfile);
+    editor.session.setMode("ace/mode/css");
+  }
+}
+
+function addjsfile() {
+  let filesdiv = document.getElementById("files");
+  let filename = prompt("Name of new Javascript file!");
+  if (filename !== null) {
+    let btn = document.createElement("button");
+    btn.className = "filebuttons";
+    btn.onclick = function () {
+      editor.setSession(jsfile);
+      editor.session.setMode("ace/mode/javascript");
+    };
+    btn.innerHTML = filename + ".js";
+    filesdiv.appendChild(btn);
+
+    let jsfile = ace.createEditSession("js");
+    editor.setSession(jsfile);
+    editor.session.setMode("ace/mode/javascript");
+  }
 }
 
 //editor.commands.addCommand({
-        //name: "addLineAfter",
-        //bindKey: {mac: "Command-Enter", win: "Ctrl-Enter"},
-        //exec: function(editor) {
-            //editor.addLineAfter();
-        //}
-      //})
+//name: "addLineAfter",
+//bindKey: {mac: "Command-Enter", win: "Ctrl-Enter"},
+//exec: function(editor) {
+//editor.addLineAfter();
+//}
+//})
